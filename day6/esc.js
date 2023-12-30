cbf9ae8674227315b6971668d528f808bb5da0c0
SujalVerman <152647296+SujalVerman@users.noreply.github.com>
2023-12-30 23:22:21 +0530

    Add files via upload

---
+++ day6/esc.js
@@ -0,0 +1,8 @@
+"use strict";
+const ps = require("prompt-sync");
+const prompt= ps();
+
+let name= prompt("what's your name:")
+console.log(`don\`suju`)
+console.log("don\"suju")
+console.log('don\'suju')
---
+++ day6/sttring.js
@@ -0,0 +1,23 @@
+"use strict";
+const ps = require("prompt-sync");
+const prompt= ps();
+
+let name = "sujal"
+console.log(name)
+
+let b= 'sujal'
+console.log(b)
+
+let c= `sujal`
+console.log(c)
+
+// all are same 
+
+let d= "suju"
+let e= "shubh"
+
+console.log(`${d} is the best frnd of ${e}`) // Output:- suju is the best frnd of shubh
+
+let f= "sujal Verman"
+console.log(f.length) // output:- 12
+
